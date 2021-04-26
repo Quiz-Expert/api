2c02e5164c2079c7b6a6b194464de7b0ddd37070
Adrian Hopek <adrian.hopek@gmail.com>
2021-04-26 21:38:28 +0200

    #7 - fix sanctum config

--- config/sanctum.php
+++ config/sanctum.php
@@ -6,10 +6,7 @@ use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 return [
-    "stateful" => explode(",", env(
-        "SANCTUM_STATEFUL_DOMAINS",
-        "localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1," . parse_url(env("APP_URL"), PHP_URL_HOST)
-    )),
+    "stateful" => "localhost",
     "expiration" => null,
     "middleware" => [
         "verify_csrf_token" => VerifyCsrfToken::class,
